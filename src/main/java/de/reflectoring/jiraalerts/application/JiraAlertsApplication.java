ed36578a8b607640139eb56cd45d76a6fd13dcdc
pDiller <Patrick.Diller55@gmail.com>
2017-06-10 12:50:49 +0200

    format code

--- src/main/java/de/reflectoring/jiraalerts/application/JiraAlertsApplication.java
+++ src/main/java/de/reflectoring/jiraalerts/application/JiraAlertsApplication.java
@@ -1,10 +1,11 @@
 package de.reflectoring.jiraalerts.application;
 
-import de.reflectoring.jiraalerts.base.JiraAlertsHomePage;
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
 
-public class JiraAlertsApplication extends WebApplication{
+import de.reflectoring.jiraalerts.base.JiraAlertsHomePage;
+
+public class JiraAlertsApplication extends WebApplication {
 
     @Override
     public Class<? extends Page> getHomePage() {
--- src/test/java/de/reflectoring/jiraalerts/base/JiraAlertsHomePageTest.java
+++ src/test/java/de/reflectoring/jiraalerts/base/JiraAlertsHomePageTest.java
@@ -1,36 +1,28 @@
 package de.reflectoring.jiraalerts.base;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.mock.MockApplication;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class JiraAlertsHomePageTest {
 
     private WicketTester wicketTester;
-    private static final Logger LOG = LoggerFactory.getLogger(JiraAlertsHomePageTest.class);
 
     @Before
-    public void setUp(){
+    public void setUp() {
         wicketTester = new WicketTester(new MockApplication());
         wicketTester.startPage(JiraAlertsHomePage.class);
     }
 
     @Test
-    public void homepageRendersSuccessfully(){
+    public void homepageRendersSuccessfully() {
         wicketTester.assertRenderedPage(JiraAlertsHomePage.class);
     }
 
     @Test
-    public void homepageShowsWelcomeLabel()   {
+    public void homepageShowsWelcomeLabel() {
         wicketTester.assertComponent("welcomeLabel", Label.class);
     }
 }
