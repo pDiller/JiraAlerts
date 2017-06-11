bf6c3d8b5e9d9f3db04a4b6fac19535386fe72a4
pDiller <Patrick.Diller55@gmail.com>
2017-06-11 13:44:13 +0200

    Configured spring with wicket. Each module got its own configuration.

--- src/main/java/de/reflectoring/jiraalerts/application/JiraAlertsApplication.java
+++ src/main/java/de/reflectoring/jiraalerts/application/JiraAlertsApplication.java
@@ -4,11 +4,18 @@ import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
 
 import de.reflectoring.jiraalerts.base.JiraAlertsHomePage;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 
 public class JiraAlertsApplication extends WebApplication {
 
     @Override
     public Class<? extends Page> getHomePage() {
         return JiraAlertsHomePage.class;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        getComponentInstantiationListeners().add(new SpringComponentInjector(this));
     }
 }
