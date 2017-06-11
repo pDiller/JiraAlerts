2471db8cdbc557c35039f7b6d9ecbd7c33388a30
pDiller <Patrick.Diller55@gmail.com>
2017-06-11 12:50:53 +0200

    Basic stuff for Jira-URL connection

---
+++ jira-component/src/main/java/de/reflectoring/jiraalerts/jiracomponent/configuration/JiraConnectionConfiguration.java
@@ -0,0 +1,37 @@
+package de.reflectoring.jiraalerts.jiracomponent.configuration;
+
+import java.io.Serializable;
+
+/**
+ * Configuration-Object for Jira-Conntection
+ */
+public class JiraConnectionConfiguration implements Serializable{
+
+    private String url;
+    private String username;
+    private String password;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ jira-component/src/main/java/de/reflectoring/jiraalerts/jiracomponent/configuration/JiraUrlConfigurationPanel.java
@@ -0,0 +1,14 @@
+package de.reflectoring.jiraalerts.jiracomponent.configuration;
+
+import org.apache.wicket.markup.html.panel.GenericPanel;
+
+/**
+ * Panel to configure the URL of JIRA instance.
+ */
+public class JiraUrlConfigurationPanel extends GenericPanel<JiraUrlConfigurationPanel> {
+
+    public JiraUrlConfigurationPanel(String id) {
+        super(id);
+    }
+
+}
