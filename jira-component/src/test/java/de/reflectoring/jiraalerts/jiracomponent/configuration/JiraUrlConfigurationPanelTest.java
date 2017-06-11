4ac4cbce4fa5c207318d13a10ebd6abe93e1e238
pDiller <Patrick.Diller55@gmail.com>
2017-06-11 12:50:53 +0200

    Basic stuff for Jira-URL connection

---
+++ jira-component/src/test/java/de/reflectoring/jiraalerts/jiracomponent/configuration/JiraUrlConfigurationPanelTest.java
@@ -0,0 +1,22 @@
+package de.reflectoring.jiraalerts.jiracomponent.configuration;
+
+import org.apache.wicket.mock.MockApplication;
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JiraUrlConfigurationPanelTest {
+
+    private WicketTester wicketTester;
+
+    @Before
+    public void setUp() {
+        wicketTester = new WicketTester(new MockApplication());
+        wicketTester.startComponentInPage(JiraUrlConfigurationPanel.class);
+    }
+
+    @Test
+    public void homepageRendersSuccessfully() {
+        wicketTester.assertComponent("", JiraUrlConfigurationPanel.class);
+    }
+}
