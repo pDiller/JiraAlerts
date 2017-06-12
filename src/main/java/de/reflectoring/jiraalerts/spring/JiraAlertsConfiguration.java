ec2061d7ec7866d9d46ecc22fa6759db143f78b8
pDiller <Patrick.Diller55@gmail.com>
2017-06-12 19:35:37 +0200

    First stable database-connection.

--- src/main/java/de/reflectoring/jiraalerts/spring/JiraAlertsConfiguration.java
+++ src/main/java/de/reflectoring/jiraalerts/spring/JiraAlertsConfiguration.java
@@ -1,11 +1,11 @@
 package de.reflectoring.jiraalerts.spring;
 
-import de.reflectoring.jiraalerts.integration.spring.IntegrationConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+import de.reflectoring.jiraalerts.integration.spring.IntegrationConfiguration;
 
 @Configuration
-@Import(IntegrationConfiguration.class)
-public class JiraAlertsConfiguration {
-}
+@Import(value = {IntegrationConfiguration.class, PersistenceContext.class})
+public class JiraAlertsConfiguration {}
