27c356e79939c0b8eed2a2e3195c1a400a9536ae
pDiller <Patrick.Diller55@gmail.com>
2017-06-15 00:20:54 +0200

    Added Tests

--- integration/src/test/java/io/reflectoring/jiraalerts/integration/homepage/HomePageTest.java
+++ integration/src/test/java/io/reflectoring/jiraalerts/integration/homepage/HomePageTest.java
@@ -1,19 +1,12 @@
 package io.reflectoring.jiraalerts.integration.homepage;
 
+import io.reflectoring.jiraalerts.integration.homepage.HomePage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import io.reflectoring.jiraalerts.integration.homepage.HomePage;
-import io.reflectoring.jiraalerts.shared.wickettests.TestConfiguration;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {TestConfiguration.class})
 public class HomePageTest {
 
     @Autowired
