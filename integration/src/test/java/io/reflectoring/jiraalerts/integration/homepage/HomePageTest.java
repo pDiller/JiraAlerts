3cabae9d99a8a44e4c97cd69ba9d656d82d14e97
pDiller <Patrick.Diller55@gmail.com>
2017-06-15 18:05:01 +0200

    fixed test

--- integration/src/test/java/io/reflectoring/jiraalerts/integration/homepage/HomePageTest.java
+++ integration/src/test/java/io/reflectoring/jiraalerts/integration/homepage/HomePageTest.java
@@ -1,18 +1,25 @@
 package io.reflectoring.jiraalerts.integration.homepage;
 
+import io.reflectoring.jiraalerts.integration.wickettests.IntegrationTestConfiguration;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.mock.MockApplication;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = IntegrationTestConfiguration.class)
 public class HomePageTest {
 
+    @Autowired
     private WicketTester wicketTester;
 
     @Before
     public void setUp() {
-        wicketTester = new WicketTester(new MockApplication());
         wicketTester.startPage(HomePage.class);
     }
 
