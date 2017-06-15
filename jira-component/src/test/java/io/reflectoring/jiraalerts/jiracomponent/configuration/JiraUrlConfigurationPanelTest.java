0da4f0b842de0c54e6352d1cc9afe3851be5ec67
pDiller <Patrick.Diller55@gmail.com>
2017-06-15 16:43:36 +0200

    Added unitTests

--- jira-component/src/test/java/io/reflectoring/jiraalerts/jiracomponent/configuration/JiraUrlConfigurationPanelTest.java
+++ jira-component/src/test/java/io/reflectoring/jiraalerts/jiracomponent/configuration/JiraUrlConfigurationPanelTest.java
@@ -1,18 +1,33 @@
 package io.reflectoring.jiraalerts.jiracomponent.configuration;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import io.reflectoring.jiraalerts.jiracomponent.wickettests.JiraComponentTestConfiguration;
 import io.reflectoring.jiraalerts.shared.wickettests.TestConfiguration;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.util.tester.FormTester;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {JiraComponentTestConfiguration.class, TestConfiguration.class})
 public class JiraUrlConfigurationPanelTest {
+
+    private static final String NEW_VALUE_TO_SAVE = "http://newValue.com";
+    private static final long JIRA_CONNECTION_DATA_ID = 1L;
+
+    @Autowired
+    private JiraConnectionConfigurationService jiraConnectionConfigurationServiceMock;
 
     @Autowired
     private WicketTester wicketTester;
@@ -25,5 +40,19 @@ public class JiraUrlConfigurationPanelTest {
     @Test
     public void homepageRendersSuccessfully() {
         wicketTester.assertComponent("", JiraUrlConfigurationPanel.class);
+        wicketTester.assertComponent("connectionUrlForm", Form.class);
+        wicketTester.assertComponent("connectionUrlForm:connectionInputField", TextField.class);
+        wicketTester.assertComponent("connectionUrlForm:submitNewConnectionUrlLink", AjaxSubmitLink.class);
+    }
+
+    @Test
+    public void formSubmitCallsServicesForSavingNewData() throws Exception {
+        FormTester formTester = wicketTester.newFormTester("connectionUrlForm");
+        formTester.setValue("connectionInputField", NEW_VALUE_TO_SAVE);
+
+        wicketTester.clickLink("connectionUrlForm:submitNewConnectionUrlLink");
+
+        verify(jiraConnectionConfigurationServiceMock, times(3)).loadConnectionUrl(JIRA_CONNECTION_DATA_ID);
+        verify(jiraConnectionConfigurationServiceMock).saveConnectionUrl(JIRA_CONNECTION_DATA_ID, NEW_VALUE_TO_SAVE);
     }
 }
