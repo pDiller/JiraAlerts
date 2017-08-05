42d069be2d8b59a200254c20a77f73b97f743f18
pDiller <Patrick.Diller55@gmail.com>
2017-08-05 12:38:54 +0200

    Logged time searching for issues

--- jira-component/src/main/java/io/reflectoring/jiraalerts/jiracomponent/issuerecognition/IssueRecognizerService.java
+++ jira-component/src/main/java/io/reflectoring/jiraalerts/jiracomponent/issuerecognition/IssueRecognizerService.java
@@ -1,9 +1,14 @@
 package io.reflectoring.jiraalerts.jiracomponent.issuerecognition;
 
+import static org.apache.commons.lang3.time.DurationFormatUtils.formatDuration;
 import static org.slf4j.LoggerFactory.getLogger;
 
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.time.StopWatch;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class IssueRecognizerService {
@@ -12,5 +17,13 @@ public class IssueRecognizerService {
 
 	public void recognize() {
 		LOGGER.info("Searching for issues");
+		StopWatch issueSearchStopWatch = new StopWatch();
+		issueSearchStopWatch.start();
+
+		//TODO Search for Issues
+		List<Object> issues = Lists.newArrayList();
+
+		issueSearchStopWatch.stop();
+		LOGGER.info("found {} issue(s) in {}.", issues.size(), formatDuration(issueSearchStopWatch.getTime(), "HH:mm:ss,SSS"));
 	}
 }
