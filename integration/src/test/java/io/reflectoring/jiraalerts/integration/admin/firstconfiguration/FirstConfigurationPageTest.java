2c6e2890fdfdbaf3e3a19d4f662321b0c5c72dad
pDiller <Patrick.Diller55@gmail.com>
2017-10-01 16:33:47 +0200

    add FirstconfigurationPanel to FIrstconfigurationPage

--- integration/src/test/java/io/reflectoring/jiraalerts/integration/admin/firstconfiguration/FirstConfigurationPageTest.java
+++ integration/src/test/java/io/reflectoring/jiraalerts/integration/admin/firstconfiguration/FirstConfigurationPageTest.java
@@ -3,6 +3,7 @@ package io.reflectoring.jiraalerts.integration.admin.firstconfiguration;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import io.reflectoring.jiraalerts.jiracomponent.admin.firstconfiguration.FirstConfigurationPanel;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,5 +29,10 @@ public class FirstConfigurationPageTest {
 	@Test
 	public void rendersSuccessfully() throws Exception {
 		wicketTester.assertRenderedPage(FirstConfigurationPage.class);
+	}
+
+	@Test
+	public void renderComponents() throws Exception {
+		wicketTester.assertComponent("firstConfigurationPanel", FirstConfigurationPanel.class);
 	}
 }
