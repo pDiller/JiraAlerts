17a768de6c975d3bfb9a5ecb1e0d2d0cf7bba500
pDiller <Patrick.Diller55@gmail.com>
2017-12-31 18:11:40 +0100

    servicemethods for storing routinequery

--- src/test/java/io/reflectoring/jiraalerts/issuerecognition/IssueRecognizerServiceTest.java
+++ src/test/java/io/reflectoring/jiraalerts/issuerecognition/IssueRecognizerServiceTest.java
@@ -1,16 +1,5 @@
 package io.reflectoring.jiraalerts.issuerecognition;
 
-import org.junit.Ignore;
-import org.junit.Test;
-
-import com.atlassian.jira.rest.client.api.JiraRestClient;
-import com.atlassian.jira.rest.client.api.RestClientException;
-import com.atlassian.jira.rest.client.api.domain.Issue;
-import com.atlassian.jira.rest.client.api.domain.SearchResult;
-import com.atlassian.util.concurrent.Promise;
-
-import io.reflectoring.jiraalerts.jiraclient.JiraRestClientService;
-
 public class IssueRecognizerServiceTest {
 
 }
