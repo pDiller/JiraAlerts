e1e16d0efb34460fa461992d2e3dbe89fcb98bc8
pDiller <Patrick.Diller55@gmail.com>
2017-12-31 17:43:00 +0100

    format

--- src/main/java/io/reflectoring/jiraalerts/dashboard/routine/RoutineQueryTablePanel.java
+++ src/main/java/io/reflectoring/jiraalerts/dashboard/routine/RoutineQueryTablePanel.java
@@ -9,18 +9,21 @@ import org.apache.wicket.model.IModel;
  */
 public class RoutineQueryTablePanel extends GenericPanel<RoutineQueryDTO> {
 
-    /**
-     * Constructor.
-     *
-     * @param id                   Wicket-ID.
-     * @param routineQueryDTOModel Model to create a new routine.
-     * @param userId               the Id of the user which is loggedin.
-     */
-    public RoutineQueryTablePanel(String id, IModel<RoutineQueryDTO> routineQueryDTOModel, long userId) {
-        super(id, routineQueryDTOModel);
+	/**
+	 * Constructor.
+	 *
+	 * @param id
+	 *            Wicket-ID.
+	 * @param routineQueryDTOModel
+	 *            Model to create a new routine.
+	 * @param userId
+	 *            the Id of the user which is loggedin.
+	 */
+	public RoutineQueryTablePanel(String id, IModel<RoutineQueryDTO> routineQueryDTOModel, long userId) {
+		super(id, routineQueryDTOModel);
 
-        add(new RoutineQueryTable("routineQueryTable", userId));
+		add(new RoutineQueryTable("routineQueryTable", userId));
 
-        add(new BookmarkablePageLink<>("createNewRoutineQueryLink", CreateRoutineQueryPage.class));
-    }
+		add(new BookmarkablePageLink<>("createNewRoutineQueryLink", CreateRoutineQueryPage.class));
+	}
 }
--- src/test/java/io/reflectoring/jiraalerts/jiraclient/JiraRestClientServiceTest.java
+++ src/test/java/io/reflectoring/jiraalerts/jiraclient/JiraRestClientServiceTest.java
@@ -1,78 +1,77 @@
 package io.reflectoring.jiraalerts.jiraclient;
 
-import com.atlassian.jira.rest.client.api.JiraRestClient;
-import io.reflectoring.jiraalerts.application.state.ApplicationStateService;
-import io.reflectoring.jiraalerts.dashboard.applicationstate.JiraConnection;
-import io.reflectoring.jiraalerts.dashboard.applicationstate.JiraConnectionRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
-
-import java.util.Collections;
-
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
 import static org.mockito.Mockito.when;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+
+import io.reflectoring.jiraalerts.application.state.ApplicationStateService;
+import io.reflectoring.jiraalerts.dashboard.applicationstate.JiraConnection;
+import io.reflectoring.jiraalerts.dashboard.applicationstate.JiraConnectionRepository;
+
 @RunWith(MockitoJUnitRunner.class)
 public class JiraRestClientServiceTest {
 
-    private static final String JIRA_USERNAME = "username";
-    private static final String JIRA_URL = "https://test.test";
-    private static final String JIRA_PASSWORD = "password";
+	private static final String JIRA_USERNAME = "username";
+	private static final String JIRA_URL = "https://test.test";
+	private static final String JIRA_PASSWORD = "password";
 
-    @Mock
-    private JiraConnectionRepository jiraConnectionRepositoryMock;
+	@Mock
+	private JiraConnectionRepository jiraConnectionRepositoryMock;
 
-    @Mock
-    private ApplicationStateService applicationStateServiceMock;
+	@Mock
+	private ApplicationStateService applicationStateServiceMock;
 
-    @InjectMocks
-    private JiraRestClientService testSubject;
+	@InjectMocks
+	private JiraRestClientService testSubject;
 
-    @Test
-    public void getJiraRestClientReturnsJiraRestClient() throws Exception {
-        JiraRestClient jiraRestClient = testSubject.getJiraRestClient(JIRA_URL, JIRA_USERNAME, JIRA_PASSWORD);
+	@Test
+	public void getJiraRestClientReturnsJiraRestClient() throws Exception {
+		JiraRestClient jiraRestClient = testSubject.getJiraRestClient(JIRA_URL, JIRA_USERNAME, JIRA_PASSWORD);
 
-        assertThat(jiraRestClient).isNotNull();
-    }
+		assertThat(jiraRestClient).isNotNull();
+	}
 
-    @Test
-    public void getInitializedJiraRestClientThrowsExceptionWhenNoJiraIsConfigured() throws Exception {
-        try {
-            testSubject.getInitializedJiraRestClient();
-            failBecauseExceptionWasNotThrown(JiraRestClientInstantiationException.class);
-        } catch (JiraRestClientInstantiationException exception) {
-            assertThat(exception.getMessage()).isEqualTo("There is no JIRA configured");
-        }
-    }
+	@Test
+	public void getInitializedJiraRestClientThrowsExceptionWhenNoJiraIsConfigured() throws Exception {
+		try {
+			testSubject.getInitializedJiraRestClient();
+			failBecauseExceptionWasNotThrown(JiraRestClientInstantiationException.class);
+		} catch (JiraRestClientInstantiationException exception) {
+			assertThat(exception.getMessage()).isEqualTo("There is no JIRA configured");
+		}
+	}
 
-    @Test
-    public void getInitializedJiraRestClientThrowsExceptionWhenNoPasswordIsSet() throws Exception {
-        JiraConnection jiraConnection = new JiraConnection();
-        when(jiraConnectionRepositoryMock.findAll()).thenReturn(singletonList(jiraConnection));
-        try {
-            testSubject.getInitializedJiraRestClient();
-            failBecauseExceptionWasNotThrown(JiraRestClientInstantiationException.class);
-        } catch (JiraRestClientInstantiationException exception) {
-            assertThat(exception.getMessage()).isEqualTo("There is no JIRA password set");
-        }
-    }
+	@Test
+	public void getInitializedJiraRestClientThrowsExceptionWhenNoPasswordIsSet() throws Exception {
+		JiraConnection jiraConnection = new JiraConnection();
+		when(jiraConnectionRepositoryMock.findAll()).thenReturn(singletonList(jiraConnection));
+		try {
+			testSubject.getInitializedJiraRestClient();
+			failBecauseExceptionWasNotThrown(JiraRestClientInstantiationException.class);
+		} catch (JiraRestClientInstantiationException exception) {
+			assertThat(exception.getMessage()).isEqualTo("There is no JIRA password set");
+		}
+	}
 
-    @Test
-    public void getInitializedJiraRestClientReturnsRestClient() throws Exception {
-        JiraConnection jiraConnection = new JiraConnection();
-        jiraConnection.setUrl(JIRA_URL);
-        jiraConnection.setUsername(JIRA_USERNAME);
-        when(jiraConnectionRepositoryMock.findAll()).thenReturn(singletonList(jiraConnection));
-        when(applicationStateServiceMock.getJiraPassword()).thenReturn(JIRA_PASSWORD);
+	@Test
+	public void getInitializedJiraRestClientReturnsRestClient() throws Exception {
+		JiraConnection jiraConnection = new JiraConnection();
+		jiraConnection.setUrl(JIRA_URL);
+		jiraConnection.setUsername(JIRA_USERNAME);
+		when(jiraConnectionRepositoryMock.findAll()).thenReturn(singletonList(jiraConnection));
+		when(applicationStateServiceMock.getJiraPassword()).thenReturn(JIRA_PASSWORD);
 
-        JiraRestClient initializedJiraRestClient = testSubject.getInitializedJiraRestClient();
+		JiraRestClient initializedJiraRestClient = testSubject.getInitializedJiraRestClient();
 
-        assertThat(initializedJiraRestClient).isNotNull();
-    }
+		assertThat(initializedJiraRestClient).isNotNull();
+	}
 }
