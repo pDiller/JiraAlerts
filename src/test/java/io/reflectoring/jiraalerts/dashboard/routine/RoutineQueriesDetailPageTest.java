5b91d62475f0eb471fa45b5ee7a5c93b60611d57
pDiller <Patrick.Diller55@gmail.com>
2018-01-11 19:10:32 +0100

    added detailstable

---
+++ src/test/java/io/reflectoring/jiraalerts/dashboard/routine/RoutineQueriesDetailPageTest.java
@@ -0,0 +1,8 @@
+package io.reflectoring.jiraalerts.dashboard.routine;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class RoutineQueriesDetailPageTest {
+
+}
