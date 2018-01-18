205d219b0ea62155787163b1358df0a81f30de39
danielvdwal <daniel.vanderwal@live.de>
2018-01-18 15:30:40 +0100

    Back to dashboard button

--- src/main/java/io/reflectoring/jiraalerts/dashboard/device/DevicesDetailPage.java
+++ src/main/java/io/reflectoring/jiraalerts/dashboard/device/DevicesDetailPage.java
@@ -1,9 +1,11 @@
 package io.reflectoring.jiraalerts.dashboard.device;
 
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
 import io.reflectoring.jiraalerts.application.JiraAlertsSession;
 import io.reflectoring.jiraalerts.base.BasePage;
+import io.reflectoring.jiraalerts.dashboard.DashboardPage;
 
 /**
  * Detail page listing all devices.
@@ -13,5 +15,7 @@ public class DevicesDetailPage extends BasePage {
 
 	public DevicesDetailPage() {
 		add(new DeviceDetailsTable("deviceTable", JiraAlertsSession.get().getUserId()));
+
+		add(new BookmarkablePageLink<>("backToDashboardLink", DashboardPage.class));
 	}
 }
