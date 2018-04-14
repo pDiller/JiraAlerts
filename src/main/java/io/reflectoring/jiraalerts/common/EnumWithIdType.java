48d0f765eeaf014179968c9d70c5d96b6a65a2b9
pDiller <Patrick.Diller55@gmail.com>
2018-04-14 09:53:35 +0200

    Updated Dependencies

--- src/main/java/io/reflectoring/jiraalerts/common/EnumWithIdType.java
+++ src/main/java/io/reflectoring/jiraalerts/common/EnumWithIdType.java
@@ -1,21 +1,14 @@
 package io.reflectoring.jiraalerts.common;
 
 import java.io.Serializable;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Types;
 
-import org.hibernate.engine.spi.SessionImplementor;
 import org.hibernate.usertype.UserType;
 
 /**
  * This {@link UserType} maps a enum of type {@link EnumWithId} to, which is defined of this enum.
- *
- * @param <T>
- *            Typ of enum (has to implement {@link EnumWithId}).
  */
-public abstract class EnumWithIdType<T extends EnumWithId> implements UserType {
+public abstract class EnumWithIdType implements UserType {
 
 	@Override
 	public Object assemble(final Serializable cached, final Object owner) {
@@ -48,35 +41,9 @@ public abstract class EnumWithIdType<T extends EnumWithId> implements UserType {
 	}
 
 	@Override
-	public Object nullSafeGet(final ResultSet rs, final String[] names, SessionImplementor session, Object owner) throws SQLException {
-		final int id = rs.getInt(names[0]);
-		if (rs.wasNull()) {
-			return null;
-		}
-		for (final EnumWithId value : returnedClass().getEnumConstants()) {
-			if (id == value.getId()) {
-				return value;
-			}
-		}
-		throw new IllegalStateException("Unknown " + returnedClass().getSimpleName() + " id:" + id);
-	}
-
-	@Override
-	public void nullSafeSet(PreparedStatement st, Object value, int index, SessionImplementor session) throws SQLException {
-		if (value == null) {
-			st.setNull(index, Types.INTEGER);
-		} else {
-			st.setInt(index, ((EnumWithId) value).getId());
-		}
-	}
-
-	@Override
 	public Object replace(final Object original, final Object target, final Object owner) {
 		return original;
 	}
-
-	@Override
-	public abstract Class<T> returnedClass();
 
 	@Override
 	public int[] sqlTypes() {
--- src/main/java/io/reflectoring/jiraalerts/dashboard/routine/RoutineQueryDataProvider.java
+++ src/main/java/io/reflectoring/jiraalerts/dashboard/routine/RoutineQueryDataProvider.java
@@ -55,6 +55,6 @@ public class RoutineQueryDataProvider extends SortableDataProvider<RoutineQueryD
 
 	private PageRequest createPageRequest(long first) {
 		Sort.Direction sortDirection = getSort().isAscending() ? Sort.Direction.ASC : Sort.Direction.DESC;
-		return new PageRequest((int) (first / rowsPerPage), rowsPerPage, sortDirection, getSort().getProperty());
+		return PageRequest.of((int) (first / rowsPerPage), rowsPerPage, sortDirection, getSort().getProperty());
 	}
 }
